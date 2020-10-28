127d970b8a30dd8a724cb453501ca4f6de6481fb
Troy Vassalotti <troyvassalotti@gmail.com>
2020-10-27 20:47:43 -0400

    almost ready for live

--- .eleventy.js
+++ .eleventy.js
@@ -43,9 +43,9 @@ module.exports = function (eleventyConfig) {
     <form action="https://frontroyalband.us17.list-manage.com/subscribe/post?u=4e13673cf275ea95a06b67103&amp;id=fc567fa48a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
         <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
-    <label class="checkbox subfield" for="gdpr_4549"><input type="checkbox" id="gdpr_4549" name="gdpr[4549]" value="Y" class="av-checkbox "><span>Emails, Please</span> </label>
+    <label class="checkbox subfield" for="gdpr_4549"><input type="checkbox" id="gdpr_4549" name="gdpr[4549]" value="Y" class="av-checkbox ">Emails, Please</label>
         </fieldset>
-    	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email address" required>
+      	   <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Type it here" required>
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4e13673cf275ea95a06b67103_fc567fa48a" tabindex="-1" value=""></div>
         <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
