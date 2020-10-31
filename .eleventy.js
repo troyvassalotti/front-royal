8551ce69fe560bb42ffa82e93d80671a62b11a95
Troy Vassalotti <troyvassalotti@gmail.com>
2020-10-31 06:21:59 -0400

    new site is ready for go live baby

--- .eleventy.js
+++ .eleventy.js
@@ -25,17 +25,10 @@ module.exports = function (eleventyConfig) {
     return `<img src="/assets/img/logo.png" alt="Front Royal logo">`
   });
 
-  eleventyConfig.addShortcode("mailchimpMin", function () {
-    return `<div id="mc_embed_signup">
-    <form action="https://frontroyalband.us17.list-manage.com/subscribe/post?u=4e13673cf275ea95a06b67103&amp;id=fc567fa48a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <div id="mc_embed_signup_scroll">
-    	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4e13673cf275ea95a06b67103_fc567fa48a" tabindex="-1" value=""></div>
-        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-        </div>
-    </form>
-    </div>`
+  eleventyConfig.addShortcode("dates", function () {
+    return `<a class="bit-widget-initializer" data-artist-name="Front Royal" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#000000" data-link-color="#0a0a0a"
+      data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-link-text-color="#f5f5f5" data-display-lineup="false" data-display-play-my-city="false" data-separator-color="rgba(255, 255, 255, 0.5)" data-display-start-time="true"
+      data-font="Open Sans" data-display-logo="false"></a>`
   });
 
   eleventyConfig.addShortcode("mailchimpGDPR", function () {
