4ae4178b2fac7091324be0350d34d55e7aad934a
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-02-02 22:38:26 -0500

    added new font and fixed some issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,16 +12,3 @@ function handleAnimatedContent(entries) {
 const animatedContentObserver = new IntersectionObserver(handleAnimatedContent);
 const contentToAnimate = document.querySelectorAll(".animated-content");
 contentToAnimate.forEach(item => animatedContentObserver.observe(item));
-
-// function handleSpinningContent(entries) {
-//   entries.map((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("spinner");
-//       spinningContentObserver.unobserve(entry.target);
-//     }
-//   })
-// }
-//
-// const spinningContentObserver = new IntersectionObserver(handleSpinningContent);
-// const contentToSpin = document.querySelectorAll(".spin-me");
-// contentToSpin.forEach(item => spinningContentObserver.observe(item));
