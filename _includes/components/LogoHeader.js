eaf3b6f4238e1d5a690c4fc2ce83e984c92117f6
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-02-28 12:43:21 -0500

    silly stuff

---
+++ _includes/components/LogoHeader.js
@@ -0,0 +1,8 @@
+const {html} = require('common-tags');
+
+module.exports = function () {
+  return html `
+  <div class="logo">
+    <a href="/"><img src="/assets/img/logo.png" alt="Front Royal logo"></a>
+  </div>`
+}
