9a8c911d7285ce8968a7d7d53d1c6f6d63b84c58
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-03-03 20:48:08 -0500

    realized my mistake - the config had requires inside the function call

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
+const componentsDir = "_includes/components";
+const Mailchimp = require(`./${componentsDir}/Mailchimp`);
+const LogoHeader = require(`./${componentsDir}/LogoHeader`);
+
 module.exports = function (eleventyConfig) {
   // Passthroughs
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("favicon.ico");
-
-  const componentsDir = "_includes/components";
-  const Mailchimp = require(`./${componentsDir}/Mailchimp`);
-  const LogoHeader = require(`./${componentsDir}/LogoHeader`);
 
   // Shortcodes
   eleventyConfig.addShortcode("mailchimpGDPR", Mailchimp);
