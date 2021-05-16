9859af156b0aa54f9dba433f16ff47c31482b020
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-05-16 19:33:50 -0400

    organizational changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,13 @@
-const componentsDir = "src/_includes/components";
 const inputDir = "src";
-const Mailchimp = require(`./${componentsDir}/Mailchimp`);
-const LogoHeader = require(`./${componentsDir}/LogoHeader`);
 
 module.exports = function (eleventyConfig) {
     // Passthroughs
     eleventyConfig.addPassthroughCopy(`${inputDir}/assets`);
     eleventyConfig.addPassthroughCopy(`${inputDir}/favicon.ico`);
 
-    // Shortcodes
-    eleventyConfig.addShortcode("mailchimpGDPR", Mailchimp);
-    eleventyConfig.addShortcode("logoHeader", LogoHeader);
-
     return {
         dir: {
-            input: 'src'
+            input: inputDir
         }
     }
 };
--- src/_includes/js/index.js
+++ src/_includes/js/index.js
@@ -1,5 +1,3 @@
-let prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
-
 function handleAnimatedContent(entries) {
     entries.map((entry) => {
         if (entry.isIntersecting) {
