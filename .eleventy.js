afbf04d9fb5e46ca64c0624fdd9231b50f14d932
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-05-21 21:07:30 -0400

    added section for covers

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     // Passthroughs
     eleventyConfig.addPassthroughCopy(`${inputDir}/assets`);
     eleventyConfig.addPassthroughCopy(`${inputDir}/favicon.ico`);
+    eleventyConfig.addPassthroughCopy(`${inputDir}/site.webmanifest`);
 
     return {
         dir: {
