ade51543c3f5dc3f4d3b140da5d5344776d032f0
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2023-04-06 23:45:28 -0400

    update gallery listing page

--- utils/filters.js
+++ utils/filters.js
@@ -2,26 +2,33 @@
  * @file Site filters
  */
 
-const metadata = require('../src/_data/metadata.json');
+const metadata = require("../src/_data/metadata.json");
+
+// Apparently you can access global data in filters but I was unable to here.
+function titleString(page, title, options = {}) {
+  if (title) {
+    if (options.pipe) {
+      return `${title} | ${metadata.title}`;
+    }
+
+    return title;
+  } else {
+    const slug = page.fileSlug;
+    const capitalized = slug.charAt(0).toUpperCase() + slug.slice(1);
+
+    return options.pipe ? `${capitalized} | ${metadata.title}` : capitalized;
+  }
+}
+
+function descriptionString(page, description) {
+  if (description) {
+    return description;
+  } else {
+    return `${titleString(page)} - a page on ${metadata.domain}`;
+  }
+}
 
 module.exports = {
-  titleString: function (title, options = {}) {
-    if (title) {
-      if (options.pipe) {
-        return `${title} | ${metadata.title}`;
-      }
-
-      return title;
-    } else {
-      const slug = page.fileSlug;
-      return slug.charAt(0).toUpperCase() + slug.slice(1);
-    }
-  },
-  descriptionString: function (description) {
-    if (description) {
-      return description;
-    } else {
-      return `${returnTitleString()} - a page on ${metadata.domain}`;
-    }
-  },
+  titleString,
+  descriptionString,
 };
