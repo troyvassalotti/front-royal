60bc15b51e865694d9f927523de5f5f052004dc8
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-02-21 21:13:11 -0500

    Update all packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,65 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const { eleventyImagePlugin } = require("@11ty/eleventy-img");
+/** @format */
 
-const FILTERS = require("./utils/filters");
-const JS_DIR = "/assets/js";
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const {eleventyImagePlugin} = require("@11ty/eleventy-img");
+const metadata = require("./src/_data/metadata.json");
+
+function titleString(page, title, options = {}) {
+	if (title) {
+		if (options.pipe) {
+			return `${title} | ${metadata.title}`;
+		}
+
+		return title;
+	} else {
+		const slug = page.fileSlug;
+		const capitalized = slug.charAt(0).toUpperCase() + slug.slice(1);
+
+		return options.pipe ? `${capitalized} | ${metadata.title}` : capitalized;
+	}
+}
+
+function descriptionString(page, description) {
+	if (description) {
+		return description;
+	} else {
+		return `${titleString(page)} - a page on ${metadata.domain}`;
+	}
+}
 
 module.exports = function (config) {
-  config.addPlugin(pluginWebc, {
-    components: [
-      "./src/_components/**/*.html",
-      "npm:@11ty/eleventy-img/*.webc",
-    ],
-  });
+	config.addPlugin(pluginWebc, {
+		components: [
+			"./src/_components/**/*.html",
+			"npm:@11ty/eleventy-img/*.webc",
+		],
+	});
 
-  // Image plugin
-  config.addPlugin(eleventyImagePlugin, {
-    formats: ["webp", "jpeg"],
-    urlPath: "/assets/img/",
+	// Image plugin
+	config.addPlugin(eleventyImagePlugin, {
+		formats: ["webp", "jpeg"],
+		urlPath: "/assets/img/",
 
-    // Notably `outputDir` is resolved automatically
-    // to the project output directory
+		// Notably `outputDir` is resolved automatically to the project output directory
 
-    defaultAttributes: {
-      loading: "lazy",
-      decoding: "async",
-    },
-  });
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
 
-  // Passthroughs
-  config.addPassthroughCopy({ public: "/" });
-  config.addPassthroughCopy({
-    "./node_modules/@zachleat/details-utils/details-utils.js": `${JS_DIR}/components/details-utils.js`,
-  });
-  config.addPassthroughCopy({
-    "./node_modules/@troyv/lightboxing/*.js": `${JS_DIR}/components/`,
-  });
-  config.addPassthroughCopy({
-    "./node_modules/@troyv/word-salad/dist/**/*.js": `${JS_DIR}/components/`,
-  });
+	// Passthroughs
+	config.addPassthroughCopy({public: "/"});
 
-  // Filters
-  Object.keys(FILTERS).forEach((filterName) => {
-    config.addFilter(filterName, FILTERS[filterName]);
-  });
+	// Apparently you can access global data in filters but I was unable to here.
+	config.addFilter("titleString", titleString);
+	config.addFilter("descriptionString", descriptionString);
 
-  return {
-    htmlTemplateEngine: "webc",
-    dir: {
-      input: "src",
-      layouts: "_includes/layouts",
-    },
-  };
+	return {
+		htmlTemplateEngine: "webc",
+		dir: {
+			input: "src",
+			layouts: "_includes/layouts",
+		},
+	};
 };
